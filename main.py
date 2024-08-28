23771050b1becacb2a13dbed09f14346f35eb77e
Samuel Henry Spink <114669576+spinks-bzz@users.noreply.github.com>
2024-08-28 10:47:59 +0200

    .

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ def load_recipe(recipes: str):
     """
     return json.loads(recipes)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Beispiel für die Datenstruktur eines Rezepts
-    RECIPE_JSON_A = '{"title": "Spaghetti Bolognese", "ingredients": '
-    RECIPE_JSON_B = '{"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    recipe_json = f"{RECIPE_JSON_A}{RECIPE_JSON_B}"
+    RECIPE_JSON_A = "{\"title\": \"Spaghetti Bolognese\", \"ingredients\": "
+    RECIPE_JSON_B = "{\"Spaghetti\": 400, \"Tomato Sauce\": 300, "
+    RECIPE_JSON_C = "\"Minced Meat\": 500}, \"servings\": 4}"
+    recipe_json = f"{RECIPE_JSON_A}{RECIPE_JSON_B}{RECIPE_JSON_C}"
     # Dein Code kommt hier hin
