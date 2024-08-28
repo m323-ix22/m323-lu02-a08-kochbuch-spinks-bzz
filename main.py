fa8777e4f85ed9832b72f05a80d0c7d6783afe5d
Samuel Henry Spink <114669576+spinks-bzz@users.noreply.github.com>
2024-08-28 10:45:14 +0200

    .

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-# Dein Code kommt hier hin
+"""
+A doc string
+"""
+import json
+
+def adjust_recipe(recipe, persons_count: int):
+    """
+    A doc string
+    """
+    recipe["servings"] = persons_count
+
+def load_recipe(recipes: str):
+    """
+    A doc string
+    """
+    return json.loads(recipes)
 
 if __name__ == '__main__':
     # Beispiel für die Datenstruktur eines Rezepts
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    RECIPE_JSON_A = '{"title": "Spaghetti Bolognese", "ingredients": '
+    RECIPE_JSON_B = '{"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    recipe_json = f"{RECIPE_JSON_A}{RECIPE_JSON_B}"
     # Dein Code kommt hier hin
